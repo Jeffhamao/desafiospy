2fa173165b180a77b1f9a1ae59458f779556f114
Jeffhamao <hamaobusiness@gmail.com>
2026-08-05 17:31:41 -0300

    Add files via upload

---
+++ arquivo/arquivo.py
@@ -0,0 +1,2 @@
+with open("teste.txt", "w") as Pasta1:
+    Pasta1.write("Primeiro texto criado em arquivo")
---
+++ arquivo/desafio2.py
@@ -0,0 +1,7 @@
+import os
+with open("Clientes.txt", "w") as financeiro:
+    financeiro.write("lista de clientes:\n Cliente 01\n Cliente 02\n Cliente 03")
+    # financeiro.write("Cliente 01\n")
+    # financeiro.write("Cliente 02\n")
+    # financeiro.write("Cliente 03\n")
+    
